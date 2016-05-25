164eef91152d2cecd3331184048b10d2ed1d7e61
Lester Estuardo Carrera <toonick.lester.estuardo@gmail.com>
2016-05-24 22:19:07 -0600

    add folder routes and index route

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function (req, res) {
-    res.send('Hola Mundo');
-});
+var routerIndex = require('./routes/index');
+
+app.use('/',routerIndex);
 
 app.listen(3000,function () {
     console.log('Servidor Corriendo en http://localhost:3000');
